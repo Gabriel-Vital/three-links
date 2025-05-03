f40006f447f60084a7dd6fb8e1ae12c77c16c287
Gabriel <gabrielvital2710@gmail.com>
2025-05-03 11:35:58 -0300

    commitzao

--- src/App.jsx
+++ src/App.jsx
@@ -3,35 +3,72 @@ import {
   InstagramLogo,
   LinkedinLogo,
   Moon,
+  Sun,
 } from "phosphor-react";
 import Button from "./components/Button";
+import { useState } from "react";
 
 function App() {
+  function getInitialDarkMode() {
+    const darkModeLocalStorage = localStorage.getItem("darkMode");
+    return darkModeLocalStorage === "true" ? true : false;
+  }
+
+  const [darkMode, setDarkMode] = useState(getInitialDarkMode);
+
+  function redirectPage(url) {
+    window.open(url, "_black");
+  }
+
+  function toggleDarkMode() {
+    setDarkMode(!darkMode);
+
+    // set in local storage
+    localStorage.setItem("darkMode", !darkMode);
+  }
+
   return (
-    <div className="w-full h-screen">
-      <header className="w-full flex justify-end p-5">
-        <button className="cursor-pointer hover:bg-gray-300 ease-in-out transition">
-          <Moon size={20} />
-        </button>
+    <div className={`w-full h-screen ${darkMode ? "bg-gray-900" : "bg-white"}`}>
+      <header
+        className={`w-full flex justify-end px-5 py-2 text-end ${
+          darkMode ? "text-white" : "text-black"
+        }`}
+      >
+        <Button variant={"secondary"} onClick={toggleDarkMode}>
+          {darkMode ? <Moon size={20} /> : <Sun size={20} />}
+        </Button>
       </header>
-      <main className="flex justify-center items-center h-full w-full">
-        <section className="border min-w-[400px] min-h-[450px] rounded-lg border-gray-400 p-6 w-fit flex flex-col justify-center items-center gap-4">
+      <main className="flex justify-center items-center h-[90%] w-full">
+        <section
+          className={`border min-w-[450px] min-h-[450px] rounded-lg border-gray-400 p-6 w-fit flex flex-col justify-center items-center gap-4 ${
+            darkMode ? "bg-gray-900 text-white" : "bg-white"
+          }`}
+        >
           <img
             className="border-3 border-gray-400 w-28 rounded-full"
             src="https://avatars.githubusercontent.com/u/177435697?v=4"
             alt="Foto de perfil do usuário"
           />
           <h1 className="font-bold text-2xl">Gabriel Vital</h1>
-          <p className="text-sm text-slate-500">Desenvolverdor FullStack</p>
+          <p className="text-sm spa text-slate-500">Desenvolvedor FullStack</p>
           <div className="w-full flex flex-col justify-center items-center gap-3">
-            <Button>
+            <Button
+              variant={"primary"}
+              onClick={() => redirectPage("https://github.com/Gabriel-Vital")}
+            >
               <InstagramLogo size={20} /> Instagram
             </Button>
-            <Button>
+            <Button
+              variant={"primary"}
+              onClick={() => redirectPage("https://digitalcollege.com.br/")}
+            >
               <LinkedinLogo size={20} />
               Linkedin
             </Button>
-            <Button>
+            <Button
+              variant={"primary"}
+              onClick={() => redirectPage("https://www.google.com.br")}
+            >
               <FacebookLogo size={20} /> Facebook
             </Button>
           </div>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,24 @@
-import { InstagramLogo } from "phosphor-react";
 import React from "react";
 
-const Button = ({ children }) => {
+const Button = ({ children, onClick, variant = "primary" }) => {
+  function getVariantClasses() {
+    switch (variant) {
+      case "primary": return "w-full flex justify-center items-center gap-5 border border-gray-300 rounded-lg px-4 py-2 cursor-pointer"
+      case "secondary": return "cursor-pointer p-2 rounded-full hover:bg-gray-300 ease-in-out transition duration-300"
+      default:
+        return "bg-yellow-300 text-white"
+    }
+  }
+
   return (
-    <button className="w-full flex justify-center items-center gap-5 border border-gray-300 rounded-lg px-4 py-2 cursor-pointer">
-      {children}
-    </button>
+    <a className="w-full" href="#">
+      <button
+        onClick={onClick}
+        className={getVariantClasses()}
+      >
+        {children}
+      </button>
+    </a>
   );
 };
 
